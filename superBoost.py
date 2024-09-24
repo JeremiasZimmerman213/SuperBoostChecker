d72174b2fd675f6c488f43f03a0950cbbd55a7e8
JeremiasZimmerman <129558381+JeremiasZimmerman213@users.noreply.github.com>
2024-09-24 09:44:33 -0400

    Update superBoost.py

--- superBoost.py
+++ superBoost.py
@@ -14,10 +14,9 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# Navigate to the page
 driver.get('https://www.bet365.com/#/HO/')
 
-# Extract content (update the selector as needed)
+# Web scrape
 try:
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, '.pbb-PopularBetBuilder-superboost'))
@@ -27,28 +26,23 @@ except:
 
 super_boosts = driver.find_elements(By.CSS_SELECTOR, '.pbb-PopularBetBuilder-superboost')
 
-# Print the content of the super boosts
+# Print super boosts
 num_boosts = 1
 if super_boosts:
     for boost in super_boosts:
         print(f'Super Boost {num_boosts}:')
         lines = boost.text.strip().split('\n')
         
-        # Remove the first line (e.g., "9.5k placed")
         lines = lines[1:]
 
-        # Print all except the last two lines (for example, event and description)
         for line in lines[:-2]:
             print(line)
 
-        # Print the last two lines with an arrow between them
         print(f"{lines[-2]} --> {lines[-1]}")
 
-        # Print separator
         print("=" * 50)
         num_boosts += 1
 else:
     print("No super boosts found!")
 
-# Close the driver
 driver.quit()
